b34ffd20465ebc9418f036c9a1237f9dc438592a
joaothimotheo <jpthimotheo@hotmail.com>
2021-06-01 18:17:49 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_range/home/home.dart';
+import 'package:flutter_range/splash/splash_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splash',
+      routes: {
+        '/splash': (BuildContext context) => SplashScreen(),
+        '/home': (BuildContext context) => HomeScreen(),
+      },
+    ),
+  );
+}
